38ed2b26466ba76b1e71974dca64de94d0eb322a
ttovarra <ttovarra@gmail.com>
2023-11-15 18:37:42 -0600

    Confirmacion 4 Beto
    Confirmacion 4 Beto

---
+++ Lista2.py
@@ -0,0 +1,18 @@
+print("Lista estudiantes")
+nombre=input("Captura nombre :")
+cali1=float(input("Captura cali 1:"))
+cali2=float(input("Captura cali 2:"))
+cali3=float(input("Captura cali 3:"))
+
+alu1=[nombre,cali1,cali2,cali3]
+
+print("Datos Alumno 1: ",alu1)
+
+nombre=input("Captura nombre :")
+cali1=float(input("Captura cali 1:"))
+cali2=float(input("Captura cali 2:"))
+cali3=float(input("Captura cali 3:"))
+
+alu2=[nombre,cali1,cali2,cali3]
+
+print("Datos Alumno 2: ",alu2)
